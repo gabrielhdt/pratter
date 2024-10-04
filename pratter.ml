1db26ae770bc1f2976d69df9a3f0e059897aa35a
Koizel <koizel.vtknz@aleeas.com>
2024-10-04 17:31:16 +0200

    Become monadic
    Use seq and not stream

--- pratter.ml
+++ pratter.ml
@@ -2,8 +2,8 @@
    Subject to the BSD-3-Clause license *)
 
 type associativity = Left | Right | Neither
-type 't error = [ `Op_conflict of 't * 't | `Too_few_arguments ]
-type 'a result = ('a, 'a error) Stdlib.result
+type 't error = [ `Op_conflict of 't | `Too_few_arguments ]
+type ('a, 'b) result = ('a, 'b error) Stdlib.result
 
 let ( >> ) f g x = g (f x)
 
@@ -43,66 +43,94 @@ module Operators = struct
     List.fold_right ( <+> ) ops none
 end
 
+type ('tok, 'a) parser = 'tok Seq.t -> ('a * 'tok Seq.t, 'tok) result
+(** Parses ['tok] to ['a]. *)
+
+let pure (x : 'b) : ('a, 'b) parser = fun strm -> Ok (x, strm)
+
+let bind (type a b) (p0 : ('tok, a) parser) (f : a -> ('tok, b) parser) :
+    ('tok, b) parser =
+ fun inp -> match p0 inp with Error _ as e -> e | Ok (out, inp') -> f out inp'
+
+let fmap (f : 'a -> 'b) (p : ('tok, 'a) parser) : ('tok, 'b) parser =
+ fun inp -> Result.map (fun (x, y) -> (f x, y)) (p inp)
+
+let fail (e : 'i error) : ('i, _) parser = fun _ -> Error e
+let push (x : 'i) : ('i, unit) parser = fun inp -> Ok ((), Seq.cons x inp)
+(* Put back a token in the stream of input. *)
+
+(** Parse a single token if there is one. *)
+let tok : ('tok, 'tok option) parser =
+ fun inp ->
+  match Seq.uncons inp with
+  | None -> Ok (None, Seq.empty)
+  | Some (t, rest) -> Ok (Some t, rest)
+
+let ( let* ) = bind
+
+let run (p : ('i, 'o) parser) (inp : _ Seq.t) : ('o, _) result =
+  Result.map fst (p inp)
+
 let expression (type a b) ~(appl : b -> b -> b) ~(token : a -> b)
-    ~(ops : (a, b) Operators.t) : a Stream.t -> b result =
+    ~(ops : (a, b) Operators.t) : (a, b) parser =
   (* [nud tbl strm t] (for null denotation) is the production of term [t] with
      {b no} left context. If [t] is not a prefix operator, [nud] is the
      identity. Otherwise, the output is a production rule. *)
-  let rec nud strm (t : a) : b result =
+  let rec nud (t : a) : (a, b) parser =
     match ops.prefix t with
-    | Some (rbp, t') ->
-        Result.map (appl t') (expression ~rbp ~rassoc:Neither strm)
-    | None -> Ok (token t)
+    | Some (rbp, t') -> fmap (appl t') (expression ~rbp ~rassoc:Neither)
+    | None -> pure (token t)
   (* [led ~strm ~left t assoc bp] is the production of term [t] with
      left context [left]. We have the invariant that [t] is a binary operator
      with associativity [assoc] and binding power [bp]. This invariant is
      ensured while called in {!val:expression}. *)
-  and led ~strm ~left t rassoc bp =
+  and led ~left t rassoc bp : (a, b) parser =
     let rbp =
       match rassoc with
       | Right -> bp *. (1. -. epsilon_float)
       | Left | Neither -> bp
     in
-    Result.map (appl (appl t left)) (expression ~rbp ~rassoc strm)
+    fmap (appl (appl t left)) (expression ~rbp ~rassoc)
   (* [expression ~rbp ~rassoc strm] parses next token of stream
      [strm] with previous operator having a right binding power [~rbp] and
      associativity [~rassoc]. *)
-  and expression ~rbp ~rassoc strm =
+  and expression ~rbp ~rassoc =
     (* [aux left] inspects the stream and may consume one of its elements, or
        return [left] unchanged. *)
-    let rec aux (left : b) =
-      match Stream.peek strm with
-      | None -> Ok left
+    let rec aux (left : b) : (a, b) parser =
+      let* next = tok in
+      match next with
+      | None -> pure left
       | Some pt -> (
           match ops.infix pt with
           | Some (lassoc, lbp, next) ->
               if lbp > rbp || (lbp = rbp && lassoc = Right && rassoc = Right)
               then
-                (* Performed before to execute side effect on stream. *)
-                let _ = Stream.next strm in
-                Result.bind (led ~strm ~left next lassoc lbp) aux
+                let* t = led ~left next lassoc lbp in
+                aux t
               else if lbp < rbp || (lbp = rbp && lassoc = Left && rassoc = Left)
-              then Ok left
-              else Error (`Op_conflict (left, next))
+              then
+                (* Put back *)
+                let* () = push pt in
+                pure left
+              else fail (`Op_conflict pt)
           | None -> (
               match ops.postfix pt with
               | Some (lbp, next) ->
-                  if lbp > rbp then
-                    (* Pop the term from the stream *)
-                    let _ = Stream.next strm in
-                    aux (appl next left)
-                  else if lbp = rbp then Error (`Op_conflict (left, next))
-                  else Ok left
+                  if lbp > rbp then aux (appl next left)
+                  else if lbp = rbp then fail (`Op_conflict pt)
+                  else
+                    let* () = push pt in
+                    pure left
               | None ->
-                  (* argument of an application *)
-                  let _ = Stream.next strm in
-                  Result.bind (nud strm pt) (fun right -> aux (appl left right))
-              ))
+                  let* right = nud pt in
+                  aux (appl left right)))
     in
-    try
-      let next = Stream.next strm in
-      let left = nud strm next in
-      Result.bind left aux
-    with Stream.Failure -> Error `Too_few_arguments
+    let* next = tok in
+    match next with
+    | None -> fail `Too_few_arguments
+    | Some next ->
+        let* left = nud next in
+        aux left
   in
   expression ~rbp:neg_infinity ~rassoc:Neither
